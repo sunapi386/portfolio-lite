b665c7f577619a9806aa671c664b40d474c45d7a
Albin Groen <albin.groen@gmail.com>
2021-03-26 17:41:17 +0100

    :bug: Purge fix

--- cv/tailwind.config.js
+++ cv/tailwind.config.js
@@ -5,13 +5,15 @@ module.exports = {
       "./pages/**/*.{js,ts,jsx,tsx}",
       "./components/**/*.{js,ts,jsx,tsx}",
     ],
-    safelist: [
-      "text-indigo-200",
-      "text-green-200",
-      "text-red-200",
-      "text-blue-200",
-      "text-yellow-200",
-    ],
+    options: {
+      safelist: [
+        "text-indigo-200",
+        "text-green-200",
+        "text-red-200",
+        "text-blue-200",
+        "text-yellow-200",
+      ],
+    },
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
