a5fcc86f80752d86163d865797c0fcb6008f195d
Albin Groen <albin.groen@gmail.com>
2021-03-26 16:36:57 +0100

    :bug: Tailwind missing styles

--- cv/tailwind.config.js
+++ cv/tailwind.config.js
@@ -1,5 +1,18 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: {
+    enabled: true,
+    content: [
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    safelist: [
+      "text-indigo-200",
+      "text-green-200",
+      "text-red-200",
+      "text-blue-200",
+      "text-yellow-200",
+    ],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
@@ -8,4 +21,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
