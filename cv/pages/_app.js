181f1e5b6217cd307e557f6b8d2d73afe956c781
Albin Groen <albin.groen@gmail.com>
2021-03-27 16:19:31 +0100

    :lipstick: Restructure content

--- cv/pages/_app.js
+++ cv/pages/_app.js
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }) {
       <div className="antialiased text-gray-900 dark:text-white dark:bg-gray-700 h-screen overflow-auto">
         <div className="absolute right-0 top-0 p-4">
           <button
-            className="px-5 py-2 rounded-full bg-gray-100 dark:bg-gray-600 cursor-pointer uppercase text-sm tracking-wider font-mono"
+            className="px-4 py-2 rounded bg-gray-100 dark:bg-gray-600 cursor-pointer uppercase text-sm tracking-wider font-mono"
             onClick={() => setDarkMode(!darkMode)}
           >
             {darkMode ? "Light mode" : "Dark mode"}
--- cv/pages/index.tsx
+++ cv/pages/index.tsx
@@ -41,23 +41,71 @@ export default function Home() {
         </header>
 
         <section className="flex flex-col lg:flex-row items-start space-y-10 md:space-x-24 md:space-y-0 mt-8">
-          <div className="flex-1 w-full">
-            <h2 className="text-3xl font-medium">Projects</h2>
+          <div className="w-3/5">
+            <h2 className="text-3xl font-medium">Work history</h2>
 
             <hr className="mt-4 dark:opacity-25 dark:border-dashed" />
 
-            {PROJECTS.map((project) => (
-              <div key={project.title} className="mt-8">
-                <h3
-                  className={`text-2xl text-${project.color} dark:text-${project.colorDark} font-bold`}
-                >
-                  {project.title}
-                </h3>
-                <p className="max-w-lg lg:max-w-md mt-2 text-lg">
-                  {project.description}
-                </p>
-              </div>
-            ))}
+            <div className="mt-8">
+              <h3 className="text-2xl font-semibold dark:font-bold">
+                Web developer
+              </h3>
+              <h4 className="mt-2 font-medium dark:text-gray-400 text-gray-500 text-lg">
+                Dooer | August 2019 - present
+              </h4>
+              <ul className="list-disc pl-5 mt-3 flex flex-col space-y-2 text-lg">
+                <li>
+                  Lead frontend development architecture and decision making in
+                  a R&D team in fall of 2020. Some of the things I introduced
+                  include a organized design system and new state management
+                  tool. This helped peers be more productive and less unsure
+                  about what technologies to use.
+                </li>
+                <li>
+                  Developed a new way for potentical customers to get a quota
+                  directly on our public website. I did this in close
+                  collaboration with a senior software engineer. This increased
+                  overall sales with about 15%.
+                </li>
+              </ul>
+            </div>
+
+            <div className="mt-8">
+              <h3 className="text-2xl font-semibold dark:font-bold">
+                Fullstack developer
+              </h3>
+              <h4 className="mt-2 font-medium dark:text-gray-400 text-gray-500 text-lg">
+                Wopify | January 2018 - July 2019
+              </h4>
+              <ul className="list-disc pl-5 mt-3 flex flex-col space-y-2 text-lg">
+                <li>
+                  Re-designed the business facing recruiting application in
+                  close contact with stakeholders such as CEO, CTO and
+                  co-founder.
+                </li>
+                <li>
+                  Architected and constructed a fully functional web application
+                  in React.js from the design specification we initially
+                  developed, in collaboration with a senior backend engineer.
+                </li>
+              </ul>
+            </div>
+
+            <div className="mt-10">
+              <h2 className="text-3xl font-medium">Skills</h2>
+
+              <hr className="mt-4 dark:opacity-25 dark:border-dashed" />
+
+              <p className="text-lg mt-6">
+                I have worked a long time with web development. Starting with
+                JavaScript HTML and CSS and learning the communication between
+                server and client, and nowadays specializing in frameworks and
+                tools like React, TypeScript, GraphQL, design-systems and state
+                management libraries. I have knowledge with a large scale of web
+                development tools and services, but can't list them all here, so
+                feel free to ask me.
+              </p>
+            </div>
 
             <div className="mt-10">
               <h2 className="text-3xl font-medium">Achievements</h2>
@@ -96,56 +144,23 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="flex-1 w-full">
-            <h2 className="text-3xl font-medium">Work history</h2>
+          <div className="w-2/5">
+            <h2 className="text-3xl font-medium">Projects</h2>
 
             <hr className="mt-4 dark:opacity-25 dark:border-dashed" />
 
-            <div className="mt-8">
-              <h3 className="text-2xl font-semibold dark:font-bold">
-                Web developer
-              </h3>
-              <h4 className="mt-2 font-medium dark:text-gray-400 text-gray-500">
-                Dooer | August 2019 - present
-              </h4>
-              <ul className="list-disc pl-5 mt-3 flex flex-col space-y-2 text-lg">
-                <li>
-                  Lead frontend development architecture and decision making in
-                  a R&D team in fall of 2020. Some of the things I introduced
-                  include a organized design system and new state management
-                  tool. This helped peers be more productive and less unsure
-                  about what technologies to use.
-                </li>
-                <li>
-                  Developed a new way for potentical customers to get a quota
-                  directly on our public website. I did this in close
-                  collaboration with a senior software engineer. This increased
-                  overall sales with 15%.
-                </li>
-              </ul>
-            </div>
-
-            <div className="mt-8">
-              <h3 className="text-2xl font-semibold dark:font-bold">
-                Fullstack developer
-              </h3>
-              <h4 className="mt-2 font-medium dark:text-gray-400 text-gray-500">
-                Wopify | January 2018 - July 2019
-              </h4>
-              <ul className="list-disc pl-5 mt-3 flex flex-col space-y-2 text-lg">
-                <li>
-                  Re-designed the business facing recruiting application in
-                  close contact with stakeholders such as CEO, CTO and
-                  co-founder.
-                </li>
-                <li>
-                  Architected and constructed a fully functional web application
-                  in React.js from the design specification we initially
-                  developed, in collaboration with a senior backend engineer.
-                </li>
-              </ul>
-            </div>
-
+            {PROJECTS.map((project) => (
+              <div key={project.title} className="mt-8">
+                <h3
+                  className={`text-xl text-${project.color} dark:text-${project.colorDark} font-bold`}
+                >
+                  {project.title}
+                </h3>
+                <p className="max-w-lg lg:max-w-md mt-2">
+                  {project.description}
+                </p>
+              </div>
+            ))}
             <div className="mt-10">
               <h2 className="text-3xl font-medium">Education</h2>
 
@@ -160,22 +175,6 @@ export default function Home() {
                   Technical degree | 2015-2018
                 </p>
               </div>
-            </div>
-
-            <div className="mt-10">
-              <h2 className="text-3xl font-medium">Skills</h2>
-
-              <hr className="mt-4 dark:opacity-25 dark:border-dashed" />
-
-              <p className="text-lg mt-6">
-                I have worked a long time with web development. Starting with
-                JavaScript HTML and CSS and learning the communication between
-                server and client, and nowadays specializing in frameworks and
-                tools like React, TypeScript, GraphQL, design-systems and state
-                management libraries. I have knowledge with a large scale of web
-                development tools and services, but can't list them all here, so
-                feel free to ask me.
-              </p>
             </div>
           </div>
         </section>
