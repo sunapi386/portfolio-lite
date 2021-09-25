52b106d58bb84124f2f2a90bbd0a84e26e82dc19
Albin Groen <albin.groen@gmail.com>
2021-09-25 15:54:52 +0200

    :mag: Update seo title

--- site/src/components/Seo.svelte
+++ site/src/components/Seo.svelte
@@ -15,10 +15,12 @@
 	export let url: string = $page.host + $page.path;
 
 	export let image: string = '/meta.png';
+
+	const fullTitle = [title, 'Albin Groen'].join(' | ');
 </script>
 
 <svelte:head>
-	<title>{title} | Albin Groen</title>
+	<title>{fullTitle}</title>
 	<meta name="description" content={description} />
 
 	<meta name="keywords" content={keywords.join(', ')} />
@@ -26,11 +28,11 @@
 
 	<meta property="og:type" content="website" />
 	<meta property="og:url" content={url} />
-	<meta property="og:title" content={title} />
+	<meta property="og:title" content={fullTitle} />
 	<meta property="og:description" content={description} />
 
 	<meta property="twitter:url" content={url} />
-	<meta property="twitter:title" content={title} />
+	<meta property="twitter:title" content={fullTitle} />
 	<meta property="twitter:description" content={description} />
 
 	<meta name="twitter:card" content="summary_large_image" />
